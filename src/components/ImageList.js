8d2e17a87032f7c3cc1b39a6c16a0d2f571b1b30
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-03-07 19:13:19 -0800

    Changed const images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,10 @@
 import './imageList.scss';
 import React from 'react';
+import ImageCard from './ImageCard';
+
 const ImageList = (props) => {
-   const images = props.images.map(({ id, description, urls }) => {
-        return (
-                <img key={id} src={urls.regular} alt={description} />
-        )
+   const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image}/>
     });
 
     return(
