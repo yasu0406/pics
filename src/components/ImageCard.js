ecc140fe68975928eb82130b1326e9ac64d2b9dd
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-03-07 19:12:51 -0800

    Created ImageCard.js

---
+++ src/components/ImageCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+    render() {
+        const { description, urls } = this.props.image;
+        return (
+            <div>
+                <img
+                    src={urls.regular}
+                    alt={description}/>
+            </div>
+        );
+    }
+}
+
+export default ImageCard;
